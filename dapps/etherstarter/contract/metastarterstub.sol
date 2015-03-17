44eb03069851a298218bcc966d3ab14ae6fcb2e5
Ralph Pichler <pichler.ralph@gmail.com>
2015-03-17 22:33:32 +0100

    release_deposit is not optional

--- dapps/etherstarter/contract/metastarterstub.sol
+++ dapps/etherstarter/contract/metastarterstub.sol
@@ -19,11 +19,11 @@ contract MetaStarterStub {
 contract MetaStarterBackend is MetaStarterStub {
     
     function get_preferred_ui () constant returns (bytes32) {}
+    function release_deposit (bytes32 id) {} // release security deposit, called by metastarter
     
     /* OPTIONAL FUNCTIONS: implemented only if the backend has the concept, unimplemented function should yield 0 */
 
     function get_deadline (bytes32 id) constant returns (uint256 deadline) { return 0; } // deadline: no more funding after this date
     function get_goal_fixed (bytes32 id) constant returns (uint256 deadline) { return 0; } // goal: goal if it is set to a fixed value
     function get_recipient (bytes32 id) constant returns (address recipient) { return 0; } // recipient: address of recipient if there is one
-    function release_deposit (bytes32 id) {} // release security deposit, called by metastarter
 }
